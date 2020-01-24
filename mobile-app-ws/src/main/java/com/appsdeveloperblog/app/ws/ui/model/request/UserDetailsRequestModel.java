a9f7b4b7d15cf8e8352cf388b28d14743022c707
Bogdan-Blaj <blajbogdan94@gmail.com>
2020-01-24 23:57:47 +0200

    initial commit

---
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,45 @@
+package com.appsdeveloperblog.app.ws.ui.model.request;
+
+public class UserDetailsRequestModel {
+
+	private String firstName;
+	
+	private String lasttName;
+	
+	private String email;
+	
+	private String password;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLasttName() {
+		return lasttName;
+	}
+
+	public void setLasttName(String lasttName) {
+		this.lasttName = lasttName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
